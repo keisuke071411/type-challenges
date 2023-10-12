c925fbd885724a78cd224eb0b93bf40986b1d38c
keisuke071411 <i.love.football0714@gmail.com>
2023-10-12 19:25:59 +0900

    Resolve: case tuple

---
+++ src/tuple.ts
@@ -0,0 +1,7 @@
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type TupleToObject<T extends readonly string[]> = {
+  [P in T[number]]: P;
+};
+
+type Result = TupleToObject<typeof tuple>;
