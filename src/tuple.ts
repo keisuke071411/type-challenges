5dee47aa4553489eb0f4548d126d262cc54daeba
keisuke071411 <i.love.football0714@gmail.com>
2024-02-07 07:53:34 +0900

    Fix: add comment to tuple challenge

--- src/tuple.ts
+++ src/tuple.ts
@@ -1,5 +1,9 @@
 const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
 
+// Tは読み取り専用の文字配列である
+// P(objectのkey)は、Tの中の値の型である
+// また、P(objectのvalue)は、P(objectのkey)と同一である
+// これによって、各値のkey/valueを持つオブジェクトの型に変換する型を作成することができる
 type TupleToObject<T extends readonly string[]> = {
   [P in T[number]]: P;
 };
